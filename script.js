2b68e720673379a6aa4e3d509a89961e202f52e4
Rubichek <rubik.serega@gmail.com>
2025-04-10 13:31:51 +0300

    Show quote and author separately and modified quotes array of strings to array of objects

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
 const quotes = [
-  "The only way to do great work is to love what you do. - Steve Jobs",
-  "Innovation distinguishes between a leader and a follower.  - Steve Jobs",
-  "Your time is limited, so don`t waste it living someone else`s life.  - Steve Jobs",
+  {
+    quote: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Your time is limited, so don’t waste it living someone else’s life',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    quote: 'I have not failed. I’ve just found 10,000 ways that won’t work',
+    author: 'Thomas Edison',
+  },
+  {
+    quote: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    quote: 'Don’t watch the clock; do what it does. Keep going',
+    author: 'Sam Levenson',
+  },
+  {
+    quote: 'The future belongs to those who believe in the beauty of their dreams',
+    author: 'Eleanor Roosevelt',
+  },
+  {
+    quote: 'It does not matter how slowly you go as long as you do not stop',
+    author: 'Confucius',
+  },
+  {
+    quote: 'Dream big and dare to fail',
+    author: 'Norman Vaughan',
+  },
 ];
 
-const quoteElement = document.getElementById("quote");
+
+const quoteElement = document.getElementById('quote');
+const quoteAuthorElement = document.getElementById('quote-author')
 const generateBtn = document.getElementById("generate-btn");
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  const { quote, author: quoteAuthor } = randomQuote; // Дистуктуризация
+  // const quote = randomQuote.quote
+  // const quoteAuthor = randomQuote.author  
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = quoteAuthor
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
